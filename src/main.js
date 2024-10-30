1f6a97f8d03ec8f3fefc470fc7896c8029a88b2f
Diovanni de Souza <diovanni@anticasper.com>
2024-10-30 16:21:35 -0300

    feat: box

--- src/main.js
+++ src/main.js
@@ -1,39 +1,43 @@
 import '@/assets/css/style.css'
 
-import SBBreadcrumb from '@/components/SBBreadcrumb.vue'
-import SBButton from '@/components/SBButton.vue'
-import SBCalendar from '@/components/SBCalendar.vue'
-import SBCard from '@/components/SBCard.vue'
-import SBCheck from '@/components/SBCheck.vue'
-import SBDate from '@/components/SBDate.vue'
-import SBForm from '@/components/SBForm.vue'
-import SBIconButton from '@/components/SBIconButton.vue'
-import SBInput from '@/components/SBInput.vue'
+import SBBadge from './components/SBBadge.vue'
+import SBBreadcrumb from './components/SBBreadcrumb.vue'
+import SBButton from './components/SBButton.vue'
+import SBCalendar from './components/SBCalendar.vue'
+import SBCard from './components/SBCard.vue'
+import SBCheck from './components/SBCheck.vue'
+import SBDate from './components/SBDate.vue'
+import SBForm from './components/SBForm.vue'
+import SBIconButton from './components/SBIconButton.vue'
+import SBInput from './components/SBInput.vue'
 import SBInputFile from '@/components/SBInputFile.vue'
-import SBInputZoom from '@/components/SBInputZoom.vue'
-import SBLabel from '@/components/SBLabel.vue'
-import SBLoader from '@/components/SBLoader.vue'
-import SBModal from '@/components/SBModal.vue'
-import SBNavButton from '@/components/SBNavButton.vue'
-import SBNavigationBar from '@/components/SBNavigationBar.vue'
-import SBRadio from '@/components/SBRadio.vue'
-import SBRadioGroup from '@/components/SBRadioGroup.vue'
-import SBSelect from '@/components/SBSelect.vue'
-import SBStatusCircle from '@/components/SBStatusCircle.vue'
-import SBStatusPill from '@/components/SBStatusPill.vue'
-import SBTable from '@/components/SBTable.vue'
-import SBTextArea from '@/components/SBTextArea.vue'
-import SBTimeline from '@/components/SBTimeline.vue'
-import SBTitle from '@/components/SBTitle.vue'
-import SBToast from '@/components/SBToast.vue'
-import SBToolTip from '@/components/SBToolTip.vue'
-import SBSpinner from '@/components/SBSpinner.vue'
+import SBInputZoom from './components/SBInputZoom.vue'
+import SBLabel from './components/SBLabel.vue'
+import SBLoader from './components/SBLoader.vue'
+import SBModal from './components/SBModal.vue'
+import SBNavButton from './components/SBNavButton.vue'
+import SBNavigationBar from './components/SBNavigationBar.vue'
+import SBPill from './components/SBPill.vue'
+import SBRadio from './components/SBRadio.vue'
+import SBRadioGroup from './components/SBRadioGroup.vue'
+import SBSelect from './components/SBSelect.vue'
+import SBSlider from './components/SBSlider.vue'
+import SBStatusCircle from './components/SBStatusCircle.vue'
+import SBStatusPill from './components/SBStatusPill.vue'
+import SBTable from './components/SBTable.vue'
+import SBTextArea from './components/SBTextArea.vue'
+import SBTimeline from './components/SBTimeline.vue'
+import SBTitle from './components/SBTitle.vue'
+import SBToast from './components/SBToast.vue'
+import SBToolTip from './components/SBToolTip.vue'
+import SBSpinner from './components/SBSpinner.vue'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App)
 
+app.component('sb-badge', SBBadge)
 app.component('sb-breadcrumb', SBBreadcrumb)
 app.component('sb-button', SBButton)
 app.component('sb-calendar', SBCalendar)
@@ -50,8 +54,10 @@ app.component('sb-loader', SBLoader)
 app.component('sb-modal', SBModal)
 app.component('sb-nav-button', SBNavButton)
 app.component('sb-navigation-bar', SBNavigationBar)
+app.component('sb-pill', SBPill)
 app.component('sb-radio', SBRadio)
 app.component('sb-radio-group', SBRadioGroup)
+app.component('sb-slider', SBSlider)
 app.component('sb-select', SBSelect)
 app.component('sb-status-circle', SBStatusCircle)
 app.component('sb-status-pill', SBStatusPill)
